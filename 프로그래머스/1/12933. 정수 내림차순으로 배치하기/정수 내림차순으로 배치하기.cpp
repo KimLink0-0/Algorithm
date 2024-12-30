d2ece7352e0c68e9d3acd6811d7e98cfa8fe394d
KimLink0-0 <linkedcirrus93@gmail.com>
2024-12-30 11:49:13 +0900

    Update 정수 내림차순으로 배치하기.cpp

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -4,6 +4,47 @@
 using namespace std;
 
 long long solution(long long n) {
+
     long long answer = 0;
+ 
+    int count = 0;
+    long long nCount = n;
+
+    vector<long long> nArray;
+
+    while (nCount > 0)
+    {
+        long long iValue = nCount % 10;
+        nArray.push_back(iValue);
+        nCount /= 10;
+        count++;
+    }
+
+    for (int j = 0; j < count; j++)
+    {
+
+        for (int k = 0; k + 1 < count; k++)
+        {
+            if (nArray[k] > nArray[k + 1])
+            {
+                continue;
+            }
+            else
+            {
+                long long temp = nArray[k];
+                nArray[k] = nArray[k + 1];
+                nArray[k + 1] = temp;
+            }
+        }
+
+    }
+
+
+    for (int l = 0; l < count; l++)
+    {
+        answer *= 10;
+        answer += nArray[l];
+    }
+
     return answer;
 }
