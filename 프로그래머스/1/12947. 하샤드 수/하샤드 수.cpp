4d458f76ba87c505988fb9fa99fda0ac1185db4d
KimLink0-0 <linkedcirrus93@gmail.com>
2024-12-31 10:52:58 +0900

    Update 하샤드 수.cpp

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -6,40 +6,36 @@ using namespace std;
 bool solution(int x) {
     bool answer = true;
 
-    // 각 자릿수의 합을 알아야 한다.
-
-    vector<int> array;
-
-
-
-    int condition = x;
+    
+    
+    // 자릿수를 계산하기
     int count = 0;
-
-    while (condition > 0)
+    int n = x;
+    
+    while (n > 0)
     {
-
-        array.push_back(condition % 10);
-        condition = condition / 10;
-        count++;
-
+        count ++;
+        n = n / 10;
     }
 
-
+    
+    
+    // 자릿수의 합을 구하기
     int sum = 0;
-
+    n = x;
+    
     for (int i = 0; i < count; i++)
     {
-
-        sum += array[i];
-
+        
+        sum += n % 10;
+        n = n / 10;
+        
     }
 
+    
 
-    // 각 자릿수의 합으로 원본 수를 나눠서 나머지가 0이 나와야한다.
-
+    // 자릿수의 합으로 원본 수를 나눠서 나머지가 0이 나와야한다
     answer = x % sum == 0 ? true : false;
-
-
 
 
 
